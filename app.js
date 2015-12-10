a00d558df3bb1e1ff6a871990479d67483c3d768
Yonah Russ <yonah@codefresh.io>
2015-12-10 15:51:56 +0200

    update express from 2.5 to 4

--- app.js
+++ app.js
@@ -1,36 +1,30 @@
 var getApp = function (config) {
 	var express = require('express');
-	var app = module.exports = express.createServer();
-
+	var app = express();
+	var bodyParser=require('body-parser');
+	var methodOverride=require('method-override');
+	var cookieParser=require('cookie-parser');
 	//middleware
 	function local_env (req, res, next){
 		res.local('real_time_server', config.server.production.real_time_server)
 		next();
 	}
 
-	app.configure(function(){
 		app.set('views', __dirname + '/views');
 		app.set('view engine', 'ejs');
-		app.register ('.html', require('ejs'));
-		app.register ('.ejs', require('ejs'));
-		app.use(express.bodyParser());
-		app.use(express.methodOverride());
-		app.use(express.cookieParser());
+		app.engine('.html', require('ejs').parseHandler);
+		app.engine('.ejs', require('ejs').parseHandler);
+		app.use(bodyParser());
+		app.use(methodOverride());
+		app.use(cookieParser());
 				
 		app.use(local_env);
-		app.use(app.router);
 		
 		var oneYear = 31557600000;
 		app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
-	});
 
-	app.configure('development', function(){
 	  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-	});
 
-	app.configure('production', function (){
-	  app.use(express.errorHandler()); 
-	});
 
 	//routes
 	require('./routes/index').configure(app);
